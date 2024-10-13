4125e2ed457b283f9bb4e71791417bd7dc0d7d28
Rosana <rosana.patino@es.schunk.com>
2024-10-13 21:09:54 +0200

    👔 basic events created

--- lib/bloc/todo/todo_event.dart
+++ lib/bloc/todo/todo_event.dart
@@ -6,3 +6,33 @@ abstract class TodoEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+//event to add task
+class AddTask extends TodoEvent {
+  final String task;
+
+  const AddTask(this.task);
+
+  @override
+  List<Object> get props => [task];
+}
+
+//event to remove task
+class RemoveTask extends TodoEvent {
+  final int taskId;
+
+  const RemoveTask(this.taskId);
+
+  @override
+  List<Object> get props => [taskId];
+}
+
+//event to update task
+class UpdateTask extends TodoEvent {
+  final int taskId;
+
+  const UpdateTask(this.taskId);
+
+  @override
+  List<Object> get props => [taskId];
+}
